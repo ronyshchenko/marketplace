79f0ba5ffdc7817fcd767f5e4381e69016d3cc1d
ronyshchenko <roma.onyshchenko@gmail.com>
2020-05-04 12:37:17 +0300

    4.05.20

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,44 @@ $(function(){
     readOnly: true
   });
 
+  $('.product-slider__inner1').slick({
+    dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+  });
+
   $('.product-slider__inner').slick({
     dots: true,
     arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 4, 
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   });
 
   var mixer = mixitup('.products__inner-box');
